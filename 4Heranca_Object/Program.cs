010cc573f86589feb88414354bac8ed2476dc1a8
crfmattos <gabrielmbserra@gmail.com>
2024-06-02 23:00:36 -0300

    soninho

---
+++ 4Heranca_Object/Program.cs
@@ -0,0 +1,25 @@
+﻿Classe1 classe = new();
+
+//nada.
+
+Console.ReadKey();
+
+public struct Struct1
+{
+
+}
+
+public class Classe1
+{
+
+}
+
+public class Classe2 //: Struct1 // Não pode herdar de outra coisa a não ser classe
+{
+
+}
+
+public class ClasseDerivada : Classe1
+{
+
+}
